227a8c531b38fbeab7c9abf3de1dceff438a8979
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2022-11-20 17:45:17 -0800

    Add makeboard.py with current board generation solution

---
+++ makeboard.py
@@ -0,0 +1,24 @@
+import random
+
+def generate_room_type():
+    random_number = random.randint(0, 100)
+    if 0 <= random_number <= 50:
+        return "Empty Room"
+    elif 51 <= random_number <= 75:
+        return "Treasure Room"
+
+def make_board(row, column):
+    board = {}
+    for k in range(row):
+        for v in range(column):
+            board.update({(k, v): generate_room_type()})
+    return board
+
+def main():
+    #rows = input("Enter the amount of rows:")
+    #columns = input("Enter the amount of columns:")
+    print(make_board())
+
+
+if __name__ == '__main__':
+    main()
