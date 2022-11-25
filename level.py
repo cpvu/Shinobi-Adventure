0a026f055f11e70f86029dd6e1e2f80116d0753d
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2022-11-24 20:11:53 -0800

    Add level.py

---
+++ level.py
@@ -0,0 +1,14 @@
+"""
+Calvin Vu
+A01291758
+
+Hanxiao Mao
+A01293003
+"""
+def assignStatus(stats, character):
+   pass
+
+def levelUp(character):
+    character["Level"] += 1
+    character["HP"] += 50
+    character["Attack"] += 25
