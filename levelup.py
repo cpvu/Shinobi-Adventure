1805eecdf66f7c91bdcc953b51977c606dcac3ce
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2022-11-26 21:21:52 -0800

    Add execute_character_glow_up with ASCII Level up art

--- levelup.py
+++ levelup.py
@@ -4,25 +4,54 @@ import random
 def assign_stats(stats):
     key_item = list(stats.keys())
     if key_item[0] == "Max HP":
-        stats[key_item[0]] += random.randint(50, 75)
+        stat_gain = random.randint(50, 75)
+        #print(f"\t \t \t {key_item[0]}: {stats[key_item[0]]} + {stat_gain } = {stats[key_item[0]] + stat_gain}")
+        stats[key_item[0]] += stat_gain
     else:
-        stats[key_item[0]] += random.randint(0, 6)
+        stat_gain = random.randint(0, 6)
+        #print(f"\t \t \t {key_item[0]}: {stats[key_item[0]]} + {stat_gain} = {stats[key_item[0]] + stat_gain}")
+        stats[key_item[0]] += stat_gain
     return stats
+
+
+def assign_experience(character):
+    remaining_experience = character["XP"] - character["XPToLevelUp"]
+
+    character["Level"] += 1
+    character["XP"] = remaining_experience
+    character["XPToLevelUp"] += 150
 
 
 def character_has_leveled(character):
     if character["XP"] >= character["XPToLevelUp"]:
         return True
 
+def execute_character_glow_up():
+    print("""            ██╗░░░░░███████╗██╗░░░██╗███████╗██╗░░░░░  ██╗░░░██╗██████╗░
+            ██║░░░░░██╔════╝██║░░░██║██╔════╝██║░░░░░  ██║░░░██║██╔══██╗
+            ██║░░░░░█████╗░░╚██╗░██╔╝█████╗░░██║░░░░░  ██║░░░██║██████╔╝
+            ██║░░░░░██╔══╝░░░╚████╔╝░██╔══╝░░██║░░░░░  ██║░░░██║██╔═══╝░
+            ███████╗███████╗░░╚██╔╝░░███████╗███████╗  ╚██████╔╝██║░░░░░
+            ╚══════╝╚══════╝░░░╚═╝░░░╚══════╝╚══════╝  ░╚═════╝░╚═╝░░░░░""")
+
 
 def main():
+    count = 0
     combat_stats = [{"HP": 100}, {"Max HP": 100}, {"Attack": 10}, {"Defense": 8}, {"Magic": 11}, {"Luck": 2}]
-    character = {"Name": "Calvin", "X": 0, "Y": 0, "Level": 1, "XP": 150, "XPToLevelUp": 100,
+    character = {"Name": "Calvin", "X": 0, "Y": 0, "Level": 1, "XP": 101, "XPToLevelUp": 100,
                  "stats": combat_stats}
+    while count < 10:
+        character["XP"] += 100
 
-    if character_has_leveled(character):
-        print("Level up!")
-        combat_stats = list(map(assign_stats, combat_stats))
+        if character_has_leveled(character):
+            execute_character_glow_up()
+            assign_experience(character)
+            print(f"\t \t \t Congratulations, you are now Level {character['Level']}!")
+            combat_stats = list(map(assign_stats, combat_stats))
+
+
+        count += 1
+
 
 
 if __name__ == '__main__':
