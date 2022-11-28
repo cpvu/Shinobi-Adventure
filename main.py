c2d49041aab5c92809c02771ddedcaa6df35efd4
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2022-11-28 01:24:35 -0800

    Refactor main game file and add import statements

--- main.py
+++ main.py
@@ -8,56 +8,9 @@ A01293003
 from makeboard import make_board
 from battle import generate_monster, battle
 from treasureroom import execute_treasure_event
-
-
-def make_character(character_name: str):
-    character = {"Name": character_name, "X": 0, "Y": 0, "Level": 1,"XP": 0, "XPToLevelUp": 100, "XP": 0, "HP": 100,
-                 "Max HP": 100,"Attack": 10, "Luck": 0}
-    return character
-
-
-def describe_current_location(board, character):
-    # we should return the whole board as a map?
-    print(f"X: {character['X']},Y: {character['Y']}")
-    print("pretend this is a map")
-
-
-def get_user_choice():
-    directions = ["North", "East", "West", "South"]
-    for direction_tuple in enumerate(directions, 1):
-        print(f"{direction_tuple[0]}: {direction_tuple[1]}", end=" | ")
-    user_choice = int(input("\nType 1 for moving North, 2 for moving East, 3 for moving West, 4 for moving South\n"))
-    while user_choice not in [1, 2, 3, 4]:
-        user_choice = int(input("Type 1 for moving North, 2 for moving East, 3 for moving West, 4 for moving South\n"))
-    direction = {k: v for (k, v) in enumerate(directions, 1)}
-    return direction[user_choice]
-
-
-def validate_move(board, character, direction):
-    if direction == "North" and (character["X"], character["Y"] + 1) in board.keys():
-        return True
-    elif direction == "South" and (character["X"], character["Y"] - 1) in board.keys():
-        return True
-    elif direction == "West" and (character["X"] - 1, character["Y"]) in board.keys():
-        return True
-    elif direction == "East" and (character["X"] + 1, character["Y"]) in board.keys():
-        return True
-    else:
-        print("Invalid action, you can't go that way!")
-        return False
-
-
-def move_character(character, direction):
-    if direction == "North":
-        character["Y"] += 1
-    elif direction == "South":
-        character["Y"] -= 1
-    elif direction == "East":
-        character["X"] += 1
-    elif direction == "West":
-        character["X"] -= 1
-    else:
-        print('Something wrong')
+from character_creation import make_character
+from character_location import describe_current_location, get_user_choice, validate_move, move_character
+from levelup import character_has_leveled, execute_character_glow_up, assign_experience, assign_stats
 
 
 def check_for_event(board, character):
@@ -78,15 +31,6 @@ def execute_event_protocol(character, event):
     else:
         print("An empty room")
         return
-
-
-def character_has_leveled(character):
-    if character["XP"] >= character["XPtoLevelUp"]:
-        return True
-
-
-def execute_glow_up_protocol():
-    pass
 
 
 def check_if_goal_attained(board, character):
@@ -111,8 +55,8 @@ def game():
             execute_event_protocol(character, check_for_event(board, character))
             if character["HP"] < 0:
                 break
-            if character_has_leveled():
-                execute_glow_up_protocol()
+            if character_has_leveled(character):
+                execute_character_glow_up()
             achieved_goal = check_if_goal_attained(board, character)
         else:
             # do something here if the move is not valid
