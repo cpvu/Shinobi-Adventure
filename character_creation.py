ede4ed4da9230b14533ba1b96cf8a37506852e56
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2022-11-28 12:45:18 -0800

    Refactor character creation to add nested stats dictionary

--- character_creation.py
+++ character_creation.py
@@ -1,7 +1,5 @@
 def make_character(character_name: str):
-    character = {"Name": character_name, "X": 0, "Y": 0, "Level": 1,"XP": 0, "XPToLevelUp": 100, "XP": 0,
-                 "HP": 150, "Max HP": 150, "Attack": 10, "Defense": 8, "Magic": 11, "Luck": 2}
+    character = [{"Name": character_name, "X": 0, "Y": 0, "Level": 1,"XP": 90, "XPToLevelUp": 100},
+                 {"stats": {"HP": 200, "Max HP": 200, "Attack": 20, "Defense": 8, "Magic": 11, "Luck": 2}}]
     return character
 
-
-combat_stats = {"stats": [{"HP": 150}, {"Max HP": 150}, {"Attack": 10}, {"Defense": 8}, {"Magic": 11}, {"Luck": 2}]}
