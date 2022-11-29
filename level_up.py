b7ff13921d48beb3ea5b54146523a315b247fa80
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2022-11-28 19:45:54 -0800

    Refactor assign stat logic

--- level_up.py
+++ level_up.py
@@ -5,16 +5,20 @@ def character_has_leveled(character):
     if character["XP"] >= character["XPToLevelUp"]:
         return True
 
+def get_character_stats(stat):
+    combat_stats = ["Max HP", "Attack", "Defense", "Magic", "Luck"]
+    if stat in combat_stats:
+        return stat
 
-def assign_stats(stat_name, stat_value):
-    if stat_name == "Max HP":
-        stat_gain = random.randint(50, 75)
-        new_stat = stat_gain + stat_value
-    else:
-        stat_gain = random.randint(2, 10)
-        new_stat = stat_gain + stat_value
-    return new_stat
 
+def assign_stats(character):
+    combat_stats = ["Max HP", "Attack", "Defense", "Magic", "Luck"]
+
+    for stat in combat_stats:
+        if stat == "Max HP":
+            character[stat] += random.randint(50, 75)
+        else:
+            character[stat] += random.randint(3, 8)
 
 def assign_experience(character):
     remaining_experience = character["XP"] - character["XPToLevelUp"]
