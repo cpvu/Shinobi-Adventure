0d28fc490719443f15c7ab56855a8eb5046df34b
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2022-11-30 12:11:40 -0800

    Add jutsu(spells) to character

--- character_creation.py
+++ character_creation.py
@@ -1,4 +1,6 @@
 def make_character(character_name: str):
-    character = {"Name": character_name, "X": 0, "Y": 0, "Level": 1, "XP": 90, "XPToLevelUp": 100,
-                 "HP": 200, "Max HP": 200, "Attack": 20, "Defense": 8, "Magic": 11, "Luck": 2}
+    character = {"Name": character_name, "X": 0, "Y": 0, "Level": 1, "XP": 0, "XPToLevelUp": 100,
+                 "HP": 200, "Max HP": 200, "Attack": 20, "Defense": 8, "Magic": 11, "Luck": 2,
+                 "Jutsu": {"Katon": [22, "Unleash a fiery blast of chakra!"]}}
     return character
+
