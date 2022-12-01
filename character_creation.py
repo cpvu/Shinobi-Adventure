11f5b2cfee4cec1b9e69308935989130586e1e33
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2022-12-01 09:47:38 -0800

    Add mana/chakra to the character

--- character_creation.py
+++ character_creation.py
@@ -1,6 +1,6 @@
 def make_character(character_name: str):
     character = {"Name": character_name, "X": 0, "Y": 0, "Level": 1, "XP": 0, "XPToLevelUp": 100,
-                 "HP": 200, "Max HP": 200, "Attack": 20, "Defense": 8, "Magic": 11, "Luck": 2,
+                 "HP": 200, "Max HP": 200, "Chakra": 100, "Max Chakra": 100, "Attack": 20, "Defense": 8, "Magic": 11, "Luck": 2,
                  "Jutsu": {("Katon", "Unleash a fiery blast of chakra!"): 22}}
     return character
 
