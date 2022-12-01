95daa57342ad57087f7291c60a8578efd0c17a57
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2022-11-30 17:37:07 -0800

    Add function to generate random jutsu and assign jutsu to character

--- treasureroom.py
+++ treasureroom.py
@@ -6,27 +6,51 @@ Hanxiao Mao
 A01293003
 """
 import random
+import time
 from battle import generate_monster, battle
+import itertools
 
 
-def treasure_chest(character):
-    print("You open the chest...")
-    print("Within are spoils of the war")
+def generate_jutsu(character = None):
+    with open("monsters/jutsus.txt") as fileobject:
+        all_jutsus = []
+        for line in fileobject.readlines():
+            jutsu_pair = line.strip().split(',')
+            all_jutsus.append((jutsu_pair[0], jutsu_pair[1]))
+        return all_jutsus[random.randint(0, len(all_jutsus) - 1)]
+
+
+def assign_jutsu(character, jutsu):
+    character["Jutsu"][jutsu] = random.randint(25, 60)
+    print(f"You have gained a new Jutsu, {jutsu[0]}")
+
+
+def open_sacred_scroll(character: dict):
+    print("You enter the room")
+    print("A sacred ninja art scroll lays bare on the floor, upon touching it you feel a new power flow into you..")
+    time.sleep(1)
+    new_jutsu = generate_jutsu(character)
+    if new_jutsu in character["Jutsu"]:
+        print("You already have this jutsu.. better luck next time.")
+
+    assign_jutsu(character, new_jutsu)
 
 
 
-def treasure_chest_battle(character):
+
+def treasure_chest_battle(character: dict):
     print("A enemy appears before you... defeat it to get the treasure")
     treasure_room_monster = generate_monster(character)
     battle(character, treasure_room_monster)
 
 
 def neutral_event(character):
-    print("You open the chest.. but theres nothing inside.")
-
+    print("You open the chest.. but theres nothing inside. Better luck next time!")
+    time.sleep(1)
 
 def treasure_chest_poisin_gas(character):
     print("You enter the room...")
+    time.sleep(1)
     print("You open the chest.")
     gas_damage = random.randint(0, 10)
     character["HP"] -= gas_damage
@@ -35,15 +59,16 @@ def treasure_chest_poisin_gas(character):
 
 def jackpot_event(character):
     print("You enter the room...")
+    time.sleep(1)
     print("You open the chest.")
     character["Attack"] += 50
-    print("You found the holy sword excalibur. A tremendous power boost.")
+    print("The most powerful jutsu of all..")
 
 
 def execute_treasure_event(character):
     random_number = random.randint(0, 100)
     if 33 >= random_number >= 0:
-        return treasure_chest(character)
+        return open_sacred_scroll(character)
     elif 66 >= random_number > 33:
         return treasure_chest_battle(character)
     elif 85 >= random_number > 66:
@@ -52,7 +77,3 @@ def execute_treasure_event(character):
         return treasure_chest_poisin_gas(character)
     elif random_number == 100:
         return jackpot_event(character)
-
-
-
-
