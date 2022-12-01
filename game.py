791ea2d7ae2b895250e7ead6815933a292b2e5e9
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2022-12-01 11:13:36 -0800

    Add game introduction

--- game.py
+++ game.py
@@ -5,12 +5,35 @@ A01291758
 Hanxiao Mao
 A01293003
 """
+import time
 from makeboard import make_board
-from battle import generate_monster, display_battle_menu, battle
+from battle import generate_monster, execute_battle_protocol
 from treasureroom import execute_treasure_event
 from character_creation import make_character
 from character_location import describe_current_location, get_user_choice, validate_move, move_character
 from level_up import character_has_leveled, execute_character_glow_up, assign_experience, assign_stats
+
+def game_introduction():
+    print(" ____ _  _ _ _  _ ____ ___  _ \n"
+          " [__  |__| | |\ | |  | |__] | \n"
+          " ___] |  | | | \| |__| |__] | \n")
+    time.sleep(2)
+    print("Welcome to the world of Shinobi, the land of ninjas and ninjitsu.")
+    time.sleep(2)
+    print("Chakra is the core of the power of ninjitsu and using powerful jutsus.")
+    time.sleep(2)
+    print("You're not from this world?")
+    time.sleep(2)
+    print("This world is a little bit different from where you came from...")
+    time.sleep(2)
+    print("The world needs your help to defeat Madara Uchiha.")
+    time.sleep(2)
+    print("Madara Uchiha is trying to put the world of Shinobi into an infinite dream state called "
+          "the Infinite Tsukuyomi.")
+    time.sleep(2)
+    print("I sense you have immense chakra, but you must first traverse through this land to "
+          "gain more power and jutsus.\n")
+    time.sleep(2)
 
 
 def game_over(character):
@@ -26,6 +49,8 @@ def check_for_event(board, character):
         return "Treasure"
     elif board[(character["X"], character["Y"])] == "Monster Room":
         return "Battle"
+    elif board[(character["X"], character["Y"])] == "Health room":
+        return "Health room"
     else:
         return "Empty Room"
 
@@ -34,12 +59,13 @@ def execute_event_protocol(character, event):
     if event == "Battle":
         monster = generate_monster(character)
         print(f"A {monster['name']} has appeared before you!")
-        battle(character, monster)
-
+        execute_battle_protocol(character, monster)
     elif event == "Treasure":
         execute_treasure_event(character)
+    elif event == "Health Room":
+        execute_treasure_event(character)
     else:
-        print("An empty room")
+        print("You've entered an empty room. There appears to be nothing in here..")
         return
 
 
@@ -51,13 +77,13 @@ def game():
     rows = 10
     columns = 10
     board = make_board(rows, columns)
-    character = make_character(input("Player Name?"))
+    game_introduction()
+    character = make_character(input("What is your name fellow shinobi?\n"))
     describe_current_location(character)
     achieved_goal = False
     while not achieved_goal:
         # prompt the current location
         # describe_current_location(board, character)
-        print(character)
         direction = get_user_choice()
         valid_move = validate_move(board, character, direction)
         if valid_move:
