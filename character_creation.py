9cc6cdc56c906df3a8ef790b15a754c12e3a0343
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2022-11-30 17:36:10 -0800

    Refactor jutsu to use a tuple as a key value

--- character_creation.py
+++ character_creation.py
@@ -1,6 +1,6 @@
 def make_character(character_name: str):
     character = {"Name": character_name, "X": 0, "Y": 0, "Level": 1, "XP": 0, "XPToLevelUp": 100,
                  "HP": 200, "Max HP": 200, "Attack": 20, "Defense": 8, "Magic": 11, "Luck": 2,
-                 "Jutsu": {"Katon": [22, "Unleash a fiery blast of chakra!"]}}
+                 "Jutsu": {("Katon", "Unleash a fiery blast of chakra!"): 22}}
     return character
 
