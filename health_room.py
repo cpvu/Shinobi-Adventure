b008bb4f509b4658000e254d4447034a9cefe3e1
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2022-12-02 03:26:47 -0800

    Add docstring to health_room functions

--- health_room.py
+++ health_room.py
@@ -2,6 +2,16 @@ import time
 
 
 def check_health_and_chakra_max(character):
+    """
+    Re-assign character health and chakra to the character's current Max HP/Max Chakra.
+
+    :param character: a dictionary object
+    :pre-condition: character must be a dictionary object containing key values: "Name" containing a string, X", "Y",
+                   "Level", "XP", "XPToLevelUp", "HP", "Chakra", "Max Chakra", "Attack", "Magic", "Luck"
+                   containing integer values, and  "Jutsu" containing a dictionary object.
+    :post-condition: Re-assign character's Max HP or Max Chakra to current HP or Chakra respectively if their values
+                     are greater than the latter.
+    """
     if character["HP"] > character["Max HP"]:
         character["HP"] = character["Max HP"]
     if character["Chakra"] > character["Max Chakra"]:
@@ -9,6 +19,14 @@ def check_health_and_chakra_max(character):
 
 
 def execute_health_room(character):
+    """
+    :param character: a dictionary object
+    :pre-condition: character must be a dictionary object containing key values: "Name" containing a string, X", "Y",
+                   "Level", "XP", "XPToLevelUp", "HP", "Chakra", "Max Chakra", "Attack", "Magic", "Luck"
+                   containing integer values, and  "Jutsu" containing a dictionary object.
+    :post-condition: Executes a game event to restore the characters healthy by re-assigning the character object's
+                    "Max HP" and "Max Chakra" key value pairs to "HP" and "Chakra" key value pairs respectively
+    """
     print("Sakura The Healer: Take a break fellow shinobi. Let me heal your wounds.\n")
     time.sleep(0.5)
     print("The shinobi healing casted a healing light onto your wounds to restore your health and chakra.")
@@ -18,5 +36,4 @@ def execute_health_room(character):
     print("Ah your Health and chakra are fully recovery now!")
     print(f'{character["Name"]} - HP: {character["HP"]}/{character["Max HP"]} Chakra:{character["Chakra"]}/'
           f'{character["Max Chakra"]}')
-
     check_health_and_chakra_max(character)
