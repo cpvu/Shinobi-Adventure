e95c0d7f57575c98b964e4ec664ea851954e9c61
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2022-12-02 03:28:11 -0800

    Add docstring for make_board and generate_room_type functions

--- makeboard.py
+++ makeboard.py
@@ -2,16 +2,36 @@ import random
 
 
 def generate_room_type():
+    """
+    Generate a random number corresponding to a string value representing a room type in the game board.
+    :post-condition: Return a string value, "Empty Room" if the random number generated is greater than or equal to 0
+                     and less than 45
+    :post-condition: Return a string value, "Treasure Room" if the random number generated is greater than or equal to
+                     45 and less than 59.
+    :post-condition: Return a string value, "Monster Room" if the random number generated is greater than or equal to
+                     60 and less than or equal to 100
+    :return: a string value, representing an empty room on the game board
+    :return: a string value, representing a treasure room on the game board
+    :return: a string value, representing a monster room on the game board
+    """
     random_number = random.randint(0, 100)
-    if 0 <= random_number <= 45:
+    if 0 <= random_number < 45:
         return "Empty Room"
-    elif 45 <= random_number <= 59:
+    elif 45 <= random_number < 59:
         return "Treasure Room"
     elif 60 <= random_number <= 100:
         return "Monster Room"
 
 
 def make_board(row, column):
+    """
+    :param row: an integer value
+    :param column: an integer value
+    :pre-condition: row must be an integer value greater than 0
+    :pre-condition: column must be an integer value greater than 0
+    :post-condition: generate a dictionary object with key pair values that represent the game board
+    :return: a dictionary object, representing the game board
+    """
     board = {(k, v): generate_room_type() for k in range(row) for v in range(column)}
     board[(0, 0)] = "Empty Room"
     board[(9, 9)] = "Boss room"
