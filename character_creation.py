cccccc8de7a0a4636bcb961f4e2917d0c8840d5b
Hanxiao Mao <hanxmao@hotmail.com>
2022-12-03 14:37:47 -0800

    Fix bug for generate_boss doctest

--- character_creation.py
+++ character_creation.py
@@ -16,13 +16,13 @@ def make_character(character_name: str) -> dict:
     :post condition: generate a dictionary that has all the stats for character
     :return: a dictionary that has all the stats for character
     >>> make_character("han")
-    {'Name': 'han', 'X': 5, 'Y': 4, 'Level': 1, 'XP': 0, 'XPToLevelUp': 100, 'HP': 200, 'Max HP': 200, 'Chakra': 100, \
-'Max Chakra': 100, 'Attack': 100, 'Magic': 11, 'Luck': 2, 'Goal Achieved': False, 'Jutsu': {('Katon', 'Unleash a fiery \
+    {'Name': 'han', 'X': 0, 'Y': 0, 'Level': 1, 'XP': 0, 'XPToLevelUp': 100, 'HP': 200, 'Max HP': 200, 'Chakra': 100, \
+'Max Chakra': 100, 'Attack': 20, 'Magic': 11, 'Luck': 2, 'Goal Achieved': False, 'Jutsu': {('Katon', 'Unleash a fiery \
 blast of chakra!', 'A powerful fire blast'): 22}}
     """
     character = {"Name": character_name,
-                 "X": 5,
-                 "Y": 4,
+                 "X": 0,
+                 "Y": 0,
                  "Level": 1,
                  "XP": 0,
                  "XPToLevelUp": 100,
@@ -30,7 +30,7 @@ blast of chakra!', 'A powerful fire blast'): 22}}
                  "Max HP": 200,
                  "Chakra": 100,
                  "Max Chakra": 100,
-                 "Attack": 100,
+                 "Attack": 20,
                  "Magic": 11,
                  "Luck": 2,
                  "Goal Achieved": False,
