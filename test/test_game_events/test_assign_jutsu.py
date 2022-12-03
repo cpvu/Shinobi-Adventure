b26c282ed66610bdcd4b07114ed2223e8179c763
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2022-12-03 11:40:24 -0800

    Refactor unit test to include test for character dictionary

--- test/test_game_events/test_assign_jutsu.py
+++ test/test_game_events/test_assign_jutsu.py
@@ -5,7 +5,6 @@ import io
 
 
 class TestAssignJutsu(TestCase):
-
     @patch('random.randint', return_value=25)
     @patch('sys.stdout', new_callable=io.StringIO)
     def test_assign_jutsu(self, mock_output, random_number_generator):
@@ -16,8 +15,12 @@ class TestAssignJutsu(TestCase):
 
         assign_jutsu(character, new_character_jutsu)
 
-        actual = mock_output.getvalue()
-        expected = "You have gained a new Jutsu, Fuuton\n"
+        expected_character_dictionary = {"Jutsu": {("Fuuton","A blast of wind emerges and blows the enemy away.",
+                               "A destructive gust of wind"): 25}}
 
-        self.assertEqual(expected, actual)
+        actual_output = mock_output.getvalue()
+        expected_output = "You have gained a new Jutsu, Fuuton\n"
+
+        self.assertEqual(expected_output, actual_output)
+        self.assertEqual(expected_character_dictionary, character)
 
