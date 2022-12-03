551835f4526c126619706b710929803d57cc37fb
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2022-12-03 13:04:55 -0800

    Add additional unit tests

--- test/test_game_events/test_assign_jutsu.py
+++ test/test_game_events/test_assign_jutsu.py
@@ -7,20 +7,57 @@ import io
 class TestAssignJutsu(TestCase):
     @patch('random.randint', return_value=25)
     @patch('sys.stdout', new_callable=io.StringIO)
-    def test_assign_jutsu(self, mock_output, random_number_generator):
+    def test_assign_jutsu_minimum_damage(self, mock_output, random_number_generator):
 
-        new_character_jutsu = ("Fuuton","A blast of wind emerges and blows the enemy away.",
+        new_character_jutsu = ("Rasengan", "Spherical chakra channels into your left arm. You enemy is hit and spirals "
+                                           "away.", "A powerful ball of spiraling chakra")
+
+        actual_character_dictionary = {"Jutsu": {}}
+        expected_character_dictionary = {"Jutsu": {("Rasengan", "Spherical chakra channels into your left arm. You "
+                                                                "enemy is hit and spirals away.",
+                                                    "A powerful ball of spiraling chakra"): 25}}
+
+        assign_jutsu(actual_character_dictionary, new_character_jutsu)
+
+        actual_output = mock_output.getvalue()
+        expected_output = "You have gained a new Jutsu, Rasengan\n"
+
+        self.assertEqual(expected_output, actual_output)
+        self.assertEqual(expected_character_dictionary, actual_character_dictionary)
+
+    @patch('random.randint', return_value=65)
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_assign_jutsu_max_damage(self, mock_output, random_number_generator):
+        new_character_jutsu = ("Fuuton", "A blast of wind emerges and blows the enemy away.",
                                "A destructive gust of wind")
-        character = {"Jutsu": {}}
 
-        assign_jutsu(character, new_character_jutsu)
+        actual_character_dictionary = {"Jutsu": {}}
+        expected_character_dictionary = {"Jutsu": {("Fuuton", "A blast of wind emerges and blows the enemy away.",
+                                                    "A destructive gust of wind"): 65}}
 
-        expected_character_dictionary = {"Jutsu": {("Fuuton","A blast of wind emerges and blows the enemy away.",
-                               "A destructive gust of wind"): 25}}
+        assign_jutsu(actual_character_dictionary, new_character_jutsu)
 
         actual_output = mock_output.getvalue()
         expected_output = "You have gained a new Jutsu, Fuuton\n"
 
         self.assertEqual(expected_output, actual_output)
-        self.assertEqual(expected_character_dictionary, character)
+        self.assertEqual(expected_character_dictionary, actual_character_dictionary)
 
+    @patch('random.randint', return_value=35)
+    @patch('sys.stdout', new_callable=io.StringIO)
+    def test_assign_jutsu_average_damage(self, mock_output, random_number_generator):
+
+        new_character_jutsu = ("Rasengan", "Spherical chakra channels into your left arm. You enemy is hit and spirals "
+                                           "away.", "A powerful ball of spiraling chakra")
+        actual_character_dictionary = {"Jutsu": {}}
+        expected_character_dictionary = {"Jutsu": {("Rasengan", "Spherical chakra channels into your left arm. You "
+                                                                "enemy is hit and spirals away.",
+                                                    "A powerful ball of spiraling chakra"): 35}}
+
+        assign_jutsu(actual_character_dictionary, new_character_jutsu)
+
+        actual_output = mock_output.getvalue()
+        expected_output = "You have gained a new Jutsu, Rasengan\n"
+
+        self.assertEqual(expected_output, actual_output)
+        self.assertEqual(expected_character_dictionary, actual_character_dictionary)
