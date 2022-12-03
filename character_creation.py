a233b9a88fdc2707dbdaf70a5ce5c3b345f28784
Hanxiao Mao <hanxmao@hotmail.com>
2022-12-03 13:04:09 -0800

    Add doctest to make_character function

--- character_creation.py
+++ character_creation.py
@@ -6,6 +6,10 @@ def make_character(character_name: str) -> dict:
     :precondition: character_name must be string type
     :post condition: generate a dictionary that has all the stats for character
     :return: a dictionary that has all the stats for character
+    >>> make_character("han")
+    {'Name': 'han', 'X': 5, 'Y': 4, 'Level': 1, 'XP': 0, 'XPToLevelUp': 100, 'HP': 200, 'Max HP': 200, 'Chakra': 100, \
+'Max Chakra': 100, 'Attack': 100, 'Magic': 11, 'Luck': 2, 'Goal Achieved': False, 'Jutsu': {('Katon', 'Unleash a fiery \
+blast of chakra!', 'A powerful fire blast'): 22}}
     """
     character = {"Name": character_name,
                  "X": 5,
