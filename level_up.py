373ec84a35aaa568c4c777701855ba4a2e9789f5
Calvin Vu <90016338+cpvu@users.noreply.github.com>
2022-12-03 10:35:07 -0800

    Add doc test for character has leveled

--- level_up.py
+++ level_up.py
@@ -9,6 +9,22 @@ def character_has_leveled(character):
     :post-condition: Return True if the value in the key "XP" is greater than the value in key "XPToLevelUp" and the
                     value in the key "Level" is less than 3 in the character dictionary object
     :return: Return True if the integer value in the key "XP" is greater than the integer value in the key "XPToLevelUp"
+
+    >>> character = {"XP": 105 , "XPToLevelUp": 100, "Level": 2}
+    >>> character_has_leveled(character)
+    True
+
+    >>> character = {"XP": 90 , "XPToLevelUp": 100, "Level": 1}
+    >>> character_has_leveled(character)
+
+
+    >>> character = {"XP": 100 , "XPToLevelUp": 100, "Level": 1}
+    >>> character_has_leveled(character)
+    True
+
+    >>> character = {"XP": 120 , "XPToLevelUp": 100, "Level": 3}
+    >>> character_has_leveled(character)
+
     """
     if character["XP"] >= character["XPToLevelUp"] and character["Level"] < 3:
         return True
@@ -54,15 +70,21 @@ def assign_experience(character):
     character["XP"] = remaining_experience
     character["XPToLevelUp"] += (character["XP"] / 2 + 100)
 
+
+
+
+def execute_character_glow_up(character):
+    """
+    Execute character level up protocol.
+
+    :param character: a dictionary object
+    :pre-condition: character must be a dictionary object containing key values: "Name" containing a string, X", "Y",
+                   "Level", "XP", "XPToLevelUp", "HP", "Chakra", "Max Chakra", "Attack", "Magic", "Luck"
+                   containing integer values, and  "Jutsu" containing a dictionary object.
+    :post-condition: Print a level up ASCII art and level up statement.
+    """
     print(f"\t \t \t Congratulations, you are now Level {character['Level']}!")
 
-
-def execute_character_glow_up():
-    """
-    Print level up ASCII art.
-
-    :post-condition: Print a level up ASCII art.
-    """
     print("""            ██╗░░░░░███████╗██╗░░░██╗███████╗██╗░░░░░  ██╗░░░██╗██████╗░
             ██║░░░░░██╔════╝██║░░░██║██╔════╝██║░░░░░  ██║░░░██║██╔══██╗
             ██║░░░░░█████╗░░╚██╗░██╔╝█████╗░░██║░░░░░  ██║░░░██║██████╔╝
